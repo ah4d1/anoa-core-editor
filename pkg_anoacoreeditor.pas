7d4c3c31f5fd8db07bde2bff49ec0db1f654d09e
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-11-01 18:57:32 +0700

    Create pkg_anoacoreeditor.pas

---
+++ pkg_anoacoreeditor.pas
@@ -0,0 +1,22 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit pkg_anoacoreeditor;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  ace_synedit, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+  RegisterUnit('ace_synedit', @ace_synedit.Register);
+end;
+
+initialization
+  RegisterPackage('pkg_anoacoreeditor', @Register);
+end.
