25f390ba6090f7ac70fc6f9354503e3158dd190a
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-11-12 13:44:53 +0700

    Update ace_synhighlighter.pas

--- ace_synhighlighter.pas
+++ ace_synhighlighter.pas
@@ -8,14 +8,16 @@ unit ace_synhighlighter;
 interface
 
 uses
-  Classes, SysUtils, SynEdit, Graphics, SynEditHighlighter, SynHighlighterCobol,
-  SynHighlighterCS, SynHighlighterCSS, SynHighlighterHTML, SynHighlighterJava,
-  SynHighlighterJSON, SynHighlighterPas, SynHighlighterPHP, SynHighlighterPython,
-  SynHighlighterSQL;
+  Classes, SysUtils, SynEdit, Graphics, SynEditHighlighter, SynHighlighterBat,
+  SynHighlighterCobol, SynHighlighterCS, SynHighlighterCSS, SynHighlighterHTML,
+  SynHighlighterIni, SynHighlighterJava, SynHighlighterJScript, SynHighlighterJSON,
+  SynHighlighterPas, SynHighlighterPHP, SynHighlighterPython, SynHighlighterRuby,
+  SynHighlighterSQL, SynHighlighterXML;
 
 type
-  TAceShLang = (aceShLangNone,aceShLangCobol,aceShLangCSharp,aceShLangCSS,aceShLangHTML,aceShLangJava,
-    aceShLangJSON,aceShLangPascal,aceShLangPHP,aceShLangPython,aceShLangSQL
+  TAceShLang = (aceShLangNone,aceShLangBatch,aceShLangCobol,aceShLangCSharp,aceShLangCSS,
+    aceShLangHTML,aceShLangIni,aceShLangJava,aceShLangJavaScript,aceShLangJSON,aceShLangPascal,
+    aceShLangPHP,aceShLangPython,aceShLangRuby,aceShLangSQL,aceShLangXML
   );
   TAceCustomSynHighlighter = class(TComponent)
   private
@@ -29,16 +31,21 @@ type
     FKeyAttriColor : TColor;
     {Highlighter}
     FNone   : TSynCustomHighLighter;
+    FBatch  : TSynBatSyn;
     FCobol  : TSynCobolSyn;
     FCSharp : TSynCSSyn;
     FCSS    : TSynCSSSyn;
     FHTML   : TSynHTMLSyn;
+    FIni    : TSynIniSyn;
     FJava   : TSynJavaSyn;
+    FJavaScript : TSynJScriptSyn;
     FJSON   : TSynJSONSyn;
     FPascal : TSynPasSyn;
     FPHP    : TSynPHPSyn;
     FPython : TSynPythonSyn;
+    FRuby   : TSynRubySyn;
     FSQL    : TSynSQLSyn;
+    FXML    : TSynXMLSyn;
     {Ext}
     FExt : string;
     {Highlighter}
@@ -60,16 +67,21 @@ type
     property vKeyAttriColor : TColor read FKeyAttriColor write FKeyAttriColor;
     {Highlighter}
     property vNone : TSynCustomHighLighter read FNone write FNone;
+    property vBatch : TSynBatSyn read FBatch write FBatch;
     property vCobol : TSynCobolSyn read FCobol write FCobol;
     property vCSharp : TSynCSSyn read FCSharp write FCSharp;
     property vCSS : TSynCSSSyn read FCSS write FCSS;
     property vHTML : TSynHTMLSyn read FHTML write FHTML;
+    property vIni : TSynIniSyn read FIni write FIni;
     property vJava : TSynJavaSyn read FJava write FJava;
+    property vJavaScript : TSynJScriptSyn read FJavaScript write FJavaScript;
     property vJSON : TSynJSONSyn read FJSON write FJSON;
     property vPascal : TSynPasSyn read FPascal write FPascal;
     property vPHP : TSynPHPSyn read FPHP write FPHP;
     property vPython : TSynPythonSyn read FPython write FPython;
+    property vRuby : TSynRubySyn read FRuby write FRuby;
     property vSQL : TSynSQLSyn read FSQL write FSQL;
+    property vXML : TSynXMLSyn read FXML write FXML;
     {}
     property vLang : TAceShLang read GetLang write SetLang default aceShLangNone;
     property vLangTxt : WideString read FLangTxt write FLangTxt;
@@ -107,32 +119,46 @@ begin
   Self.FKeyAttriColor := clBlue;
   {Highlighter}
   Self.FNone   := nil;
+  Self.FBatch  := TSynBatSyn.Create(Self);
   Self.FCobol  := TSynCobolSyn.Create(Self);
   Self.FCSharp := TSynCSSyn.Create(Self);
   Self.FCSS    := TSynCSSSyn.Create(Self);
   Self.FHTML   := TSynHTMLSyn.Create(Self);
+  Self.FIni    := TSynIniSyn.Create(Self);
   Self.FJava   := TSynJavaSyn.Create(Self);
+  Self.FJavaScript := TSynJScriptSyn.Create(Self);
   Self.FJSON   := TSynJSONSyn.Create(Self);
   Self.FPascal := TSynPasSyn.Create(Self);
   Self.FPHP    := TSynPHPSyn.Create(Self);
   Self.FPython := TSynPythonSyn.Create(Self);
+  Self.FRuby   := TSynRubySyn.Create(Self);
   Self.FSQL    := TSynSQLSyn.Create(Self);
+  Self.FXML    := TSynXMLSyn.Create(Self);
   {Lang}
-  Self.vLangTxt := '|COBOL|C#|CSS|HTML|Java|JSON|Pascal|PHP|Python|SQL';
-  Self.vLangResWordFile := 'none|cobol|cs|css|html|java|json|pascal|php|python|sql';
+  Self.vLangTxt := '|Batch|COBOL|C#|CSS|HTML|INI|Java|JavaScript|JSON|Pascal|'
+    + 'PHP|Python|Ruby|SQL|XML'
+  ;
+  Self.vLangResWordFile := 'none|batch|cobol|cs|css|html|ini|java|javascript|json|pascal'
+    + '|php|python|ruby|sql|xml'
+  ;
   Self.vLangs := vacString.fcSplit(string(Self.vLangResWordFile),Char('|'));
   Self.vReservedWords := TStringList.Create;
   {Set Color}
+  Self.fcSetColor(Self.FBatch);
   Self.fcSetColor(Self.FCobol);
   Self.fcSetColor(Self.FCSharp);
   Self.fcSetColor(Self.FCSS);
   Self.fcSetColor(Self.FHTML);
+  Self.fcSetColor(Self.FIni);
   Self.fcSetColor(Self.FJava);
+  Self.fcSetColor(Self.FJavaScript);
   Self.fcSetColor(Self.FJSON,False);
   Self.fcSetColor(Self.FPascal);
   Self.fcSetColor(Self.FPHP);
   Self.fcSetColor(Self.FPython);
+  Self.fcSetColor(Self.FRuby);
   Self.fcSetColor(Self.FSQL);
+  Self.fcSetColor(Self.FXML);
 end;
 
 function TAceCustomSynHighlighter.GetLang : TAceShLang;
@@ -145,16 +171,21 @@ begin
   Self.FLang := AValue;
   case Self.FLang of
     aceShLangNone   : Self.FHighlighter := Self.FNone;
+    aceShLangBatch  : Self.FHighlighter := Self.FBatch;
     aceShLangCobol  : Self.FHighlighter := Self.FCobol;
     aceShLangCSharp : Self.FHighlighter := Self.FCSharp;
     aceShLangCSS    : Self.FHighlighter := Self.FCSS;
     aceShLangHTML   : Self.FHighlighter := Self.FHTML;
+    aceShLangIni    : Self.FHighlighter := Self.FIni;
     aceShLangJava   : Self.FHighlighter := Self.FJava;
+    aceShLangJavaScript : Self.FHighlighter := Self.FJavaScript;
     aceShLangJSON   : Self.FHighlighter := Self.FJSON;
     aceShLangPascal : Self.FHighlighter := Self.FPascal;
     aceShLangPHP    : Self.FHighlighter := Self.FPHP;
     aceShLangPython : Self.FHighlighter := Self.FPython;
+    aceShLangRuby   : Self.FHighlighter := Self.FRuby;
     aceShLangSQL    : Self.FHighlighter := Self.FSQL;
+    aceShLangXML    : Self.FHighlighter := Self.FXML;
   end;
 end;
 
@@ -178,16 +209,21 @@ function TAceCustomSynHighlighter.GetDefaultFilter : WideString;
 begin
   Result := ''
     + 'All Files (*.*)|*.*'
+    + '|' + Self.FBatch.DefaultFilter
     + '|' + Self.FCobol.DefaultFilter
     + '|' + Self.FCSharp.DefaultFilter
     + '|' + Self.FCSS.DefaultFilter
     + '|' + Self.FHTML.DefaultFilter
+    + '|' + Self.FIni.DefaultFilter
     + '|' + Self.FJava.DefaultFilter
+    + '|' + Self.FJavaScript.DefaultFilter
     + '|' + Self.FJSOn.DefaultFilter
     + '|' + Self.FPascal.DefaultFilter
     + '|' + Self.FPHP.DefaultFilter
     + '|' + Self.FPython.DefaultFilter
+    + '|' + Self.FRuby.DefaultFilter
     + '|' + Self.FSQL.DefaultFilter
+    + '|' + Self.FXML.DefaultFilter
   ;
 end;
 
@@ -201,24 +237,31 @@ var
   LLang : TAceShLang;
 begin
   LLang := aceShLangNone;
-  if vacFileDir.fcIsExt(AValue,Self.FCobol.DefaultFilter) then LLang := aceShLangCobol
+  if vacFileDir.fcIsExt(AValue,Self.FBatch.DefaultFilter) then LLang := aceShLangBatch
+    else if vacFileDir.fcIsExt(AValue,Self.FCobol.DefaultFilter) then LLang := aceShLangCobol
     else if vacFileDir.fcIsExt(AValue,Self.FCSharp.DefaultFilter) then LLang := aceShLangCSharp
     else if vacFileDir.fcIsExt(AValue,Self.FCSS.DefaultFilter) then LLang := aceShLangCSS
     else if vacFileDir.fcIsExt(AValue,Self.FHTML.DefaultFilter) then LLang := aceShLangHTML
+    else if vacFileDir.fcIsExt(AValue,Self.FIni.DefaultFilter) then LLang := aceShLangIni
     else if vacFileDir.fcIsExt(AValue,Self.FJava.DefaultFilter) then LLang := aceShLangJava
+    else if vacFileDir.fcIsExt(AValue,Self.FJavaScript.DefaultFilter) then LLang := aceShLangJavaScript
     else if vacFileDir.fcIsExt(AValue,Self.FJSON.DefaultFilter) then LLang := aceShLangJSON
     else if vacFileDir.fcIsExt(AValue,Self.FPascal.DefaultFilter) then LLang := aceShLangPascal
     else if vacFileDir.fcIsExt(AValue,Self.FPHP.DefaultFilter) then LLang := aceShLangPHP
     else if vacFileDir.fcIsExt(AValue,Self.FPython.DefaultFilter) then LLang := aceShLangPython
+    else if vacFileDir.fcIsExt(AValue,Self.FRuby.DefaultFilter) then LLang := aceShLangRuby
     else if vacFileDir.fcIsExt(AValue,Self.FSQL.DefaultFilter) then LLang := aceShLangSQL
+    else if vacFileDir.fcIsExt(AValue,Self.FXML.DefaultFilter) then LLang := aceShLangXML
   ;
   Self.FLang := LLang;
 end;
 
 procedure TAceCustomSynHighlighter.fcSetColor (ALang : TSynCustomHighlighter);
 begin
-  ALang.CommentAttribute.Foreground := Self.FCommentAttriColor;
   ALang.KeywordAttribute.Foreground := Self.FKeyAttriColor;
+  ALang.KeywordAttribute.Style := [fsBold];
+  ALang.CommentAttribute.Foreground := Self.FCommentAttriColor;
+  ALang.CommentAttribute.Style := [fsItalic];
 end;
 
 procedure TAceCustomSynHighlighter.fcSetColor (ALang : TSynCustomHighlighter; AComment : Boolean);
